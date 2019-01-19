3700ddbd0284a5cdde99313251c199f0b61e79eb
trmasha2002 <trmasha2015@yandex.ru>
2019-01-19 15:12:20 +0000

    .....

--- web/forms.py
+++ web/forms.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SelectField, SelectMultipleField, SubmitField, HiddenField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
