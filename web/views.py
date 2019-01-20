7252d1cd5ba008524a72496c88e199795d601411
Мария Тряпицына <mariatrapicyna@MacBook-Pro-Maria-3.local>
2019-01-21 00:49:15 +0300

    add my_ideas

--- web/views.py
+++ web/views.py
@@ -23,24 +23,18 @@ auth = HTTPBasicAuth()
 @app.route("/ideas", methods=['POST'])
 def add_idea():
     name = request.json['name']
+    token = request.json['token']
+    user = db.session.query(User).filter(User.token == token).one()
     small_description = request.json['small_description']
     description = request.json['description']
     image = request.json['image']
-
     idea = Idea(name, small_description, description, image)
-    #idea.author_id = session['id']
-    #idea.like()
-    #print(session['id'])
+    idea.author_id = user.id
+    idea_schema = IdeaSchema()
     db.session.add(idea)
     db.session.commit()
-    idea_schema = IdeaSchema()
 
     return idea_schema.jsonify(idea)
-    #if form.validate_on_submit():
-    #    idea = Idea(form.name.data, form.small_description.data, form.description.data, form.image.data)
-    #    id = idea.save()
-    #    return redirect('/')
-    #return render_template("add_idea.html", form=form)
 
 @app.route('/ideas/<int:idea_id>', methods=['GET'])
 def get_by_id_idea(idea_id):
@@ -50,7 +44,9 @@ def get_by_id_idea(idea_id):
 
 @app.route('/my_ideas', methods=['GET'])
 def get_my_ideas():
-    ideas = db.session.query(Idea).filter(Idea.author_id == session['id']).all()
+    token = request.json['token']
+    user = db.session.query(User).filter(User.token == token).one()
+    ideas = db.session.query(Idea).filter(Idea.author_id == user.id).all()
     ideas_schema = IdeaSchema(many=True)
     result = ideas_schema.dump(ideas)
     return jsonify(result.data)
@@ -66,12 +62,7 @@ def get_favorites():
 def get_favorite_by_idea(idea_id):
     idea = get_by_id_idea(idea_id)
 
-@app.route('/my_ideas/<int:idea_id>', methods=['GET'])
-def get_my_idea_id(idea_id):
-    idea = Idea.get_by_id(idea_id)
-    idea_schema = IdeaSchema()
-    result = idea_schema.dump(idea)
-    return jsonify(result.data)
+
 
 @app.route('/ideas/<int:idea_id>', methods=['PUT'])
 def update_idea(idea_id):
@@ -150,6 +141,7 @@ def get_sessions():
     sessions_schema = SessionSchema(many=True)
     result = sessions_schema.dump(sessions)
     return jsonify(result.data)
+
 @app.route('/users', methods=['PUT'])
 def update_user():
     user_schema = UserSchema()
@@ -169,11 +161,8 @@ def update_user():
     user.specialization = specialization
     user.description = description
     user.image = image
-
     db.session.commit()
-
     return user_schema.jsonify(user)
-
 @app.route('/users', methods=['DELETE'])
 def delete_user():
     token = request.json['token']
